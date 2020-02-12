60506bf9047a5e5c3588c2f703dcb92cc4cbfa98
BouriauB <47318623+BouriauB@users.noreply.github.com>
2020-02-12 11:33:45 +0100

    View home.blade.php: create 'home' part

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,41 @@
     <title>Laravel</title>
 
     <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=The+Girl+Next+Door&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/phone.css') }}" rel="stylesheet">
 
 </head>
 <body>
-    <div>
-        test
+    <div class="parallax_top" data-parallax="scroll" data-image-src="{{ asset('images/nazareno-contreras-p0NvD6umfgY-unsplash.jpg') }}">
+        <header>
+            <nav>
+                <ul id="header-name">
+                    <li>La Fabrique .</li>
+                </ul>
+                <ul id="menu">
+                    <li><a href="#">Présentation</a></li>
+                    <li><a href="#">Réalisations</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div id="container-name">
+            <div id="block-name">
+                <ul>
+                    <li id="firstname">La</li>
+                    <li id="lastname">Fabrique</li>
+                    <li id="slogan">No Game, No Bar</li>
+                </ul>
+            </div>
+        </div>
     </div>
+    <div id="about-container">
+
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js([
+    'resources/js/app.js',
+    'resources/js/parallax.js'
+    ], 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/phone.scss', 'public/css');
+
