4d820734981cdd24cbc290cfab151015876e9a75
BouriauB <47318623+BouriauB@users.noreply.github.com>
2020-03-10 16:50:35 +0100

    New table: Products

---
+++ database/migrations/2020_03_10_152440_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('product_id');
+            $table->string('product_name');
+            $table->longText('product_description');
+            $table->float('product_prize',10, 2);
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')
+                ->references('category_id')->on('categories')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
