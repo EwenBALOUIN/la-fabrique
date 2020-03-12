36cdf84d4ea3474c0b0d49940acdd0a71929870d
BouriauB <47318623+BouriauB@users.noreply.github.com>
2020-03-12 20:39:31 +0100

    Add some css to views

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.products')
 
 @section('content')
-    @foreach($categories as $category)
-        <h1><a href="{{route('prod.show', $category->category_id)}}">{{ $category->category_name }}</a></h1>
-        <p><a href="{{ route('cat.edit', $category->category_id) }}">Editer</a></p>
-    @endforeach
+    <div class="backend_container">
+        <div class="categories_container">
+            <h1>Catégorie</h1>
+            @foreach($categories as $category)
+                <a href="{{route('prod.show', $category->category_id)}}">
+                    <div class="category_container">
+                        <h2>{{ $category->category_name }}</h2>
+                        {{--                <p><a href="{{ route('cat.edit', $category->category_id) }}">Editer</a></p>--}}
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    </div>
 @endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.products')
 
 @section('content')
-    @foreach($products as $product)
-        <h1>{{$product->category_id}}.{{ $product->product_name }}</h1>
-        <p>{{ $product->product_description }}</p>
-        <p><a href="{{ route('prod.edit', [$product->product_id]) }}">Editer</a></p>
-
-    @endforeach
-
+    <div class="backend_container">
+        <h1>Catégorie</h1>
+        <div class="products_container">
+            @foreach($products as $product)
+                <div class="product_container">
+                    <h2>{{$product->category_id}}.{{ $product->product_name }}</h2>
+                    <p>{{ $product->product_description }}</p>
+                    <p><a href="{{ route('prod.edit', [$product->product_id]) }}">Editer</a></p>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @stop
