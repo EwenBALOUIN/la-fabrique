b9ad63cb1ab8a494401e13ef793b9d89998f4a6d
Lucas Durand <47317956+Lucasdnd@users.noreply.github.com>
2020-03-13 10:21:09 +0100

    Création des roles Admin, Author et User

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();   
+
+        $adminRole = Role::where('name','admin') ->first();
+        $authorRole = Role::where('name','author') ->first();
+        $userRole = Role::where('name','user') ->first();
+
+        $admin = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin')
+        ]);
+
+        $author = User::create([
+            'name' => 'Author',
+            'email' => 'author@author.com',
+            'password' => bcrypt('author')
+        ]);
+
+        $user = User::create([
+            'name' => 'User',
+            'email' => 'user@user.com',
+            'password' => bcrypt('user')
+        ]);
+
+        $admin->roles()->attach($adminRole);
+        $author->roles()->attach($authorRole);
+        $user->roles()->attach($userRole);
+    }
+}
